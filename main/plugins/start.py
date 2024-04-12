5434cf05365dfda8947e8a8a776c67c798bdc389
uncreations2000 <155283951+uncreations2000@users.noreply.github.com>
2024-04-12 06:10:02 -0700

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,6 +1,4 @@
 #Github.com/Vasusen-code
-import pyrogram
-from pyrogram import Client, filters
 import os
 from .. import bot as Drone
 from telethon import events, Button
@@ -8,10 +6,6 @@ from telethon import events, Button
 from ethon.mystarts import start_srb
     
 S = '/' + 's' + 't' + 'r' + 't'
-@Drone.on(filters.command(["start"]))
-def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link__\n\n{USAGE}",
-                     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🌐 Source Code", url="https://www.bing.com/chat")]]), reply_to_message_id=message.id)
 
 
 @Drone.on(events.callbackquery.CallbackQuery(data="set"))
